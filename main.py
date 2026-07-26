a30d46a47d042c55706840e1ffacf893e7f5698c
AnkurKulhari <ankurkulhari16@gmail.com>
2026-07-26 13:52:57 +0530

    Allow Vercel frontend in CORS

--- main.py
+++ main.py
@@ -12,9 +12,15 @@ app = FastAPI()
 app.include_router(auth.router)
 app.include_router(todos.router)
 
+origins = [
+    "http://localhost:5173",
+    "https://todo-flow-fullstack-app-git-main-ankurkulharis-projects.vercel.app",
+    "https://todo-flow-fullstack-9wmpo7d1e-ankurkulharis-projects.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
